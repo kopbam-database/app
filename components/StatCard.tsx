2fa7dfb06fed493a3bd1c6109ad56941c31cf125
kopbam-database <koperasibanuamamase@gmail.com>
2026-02-04 21:19:47 +0800

    Update StatCard.tsx

--- components/StatCard.tsx
+++ components/StatCard.tsx
@@ -37,6 +37,9 @@ export const Icons = {
     </svg>
   ),
 
+  export default StatCard;
+
+
   Payment: () => (
     <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
       <path
